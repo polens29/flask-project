f5358d739fbfa31e67f5e63d50151b27bada993b
Pauline Añasco <polens29@gmail.com>
2018-01-19 10:14:36 +0800

    changes in db

--- SocialWeaver/models.py
+++ SocialWeaver/models.py
@@ -2,7 +2,7 @@ from database import db
 
 
 class Event(db.Model):
-	id = db.Column('student_id', db.Integer, primary_key = True)
+	id = db.Column('event_id', db.Integer, primary_key=True)
 	name = db.Column(db.String(100))
 	description = db.Column(db.String(50))
 	location = db.Column(db.String(200))
